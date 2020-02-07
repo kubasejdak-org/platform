464d29e5570c29e061c5c8f787192b5730e57007
Kuba Sejdak <kuba.sejdak@gmail.com>
2020-02-07 14:01:06 +0100

    Fix CI.

--- tools/build/linux-arm-gcc-debug.sh
+++ tools/build/linux-arm-gcc-debug.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-conan install .. --build missing -pr arm-linux-gnueabihf-gcc-8 -s build_type=Debug
+conan install .. --build missing -pr arm-linux-gnueabihf-gcc-9 -s build_type=Debug
 cmake .. -DPLATFORM=linux-arm
 make
