4db9784f291b56601b2f26537e9ed697fad0c6a7
Kuba Sejdak <kuba.sejdak@gmail.com>
2020-02-07 21:53:53 +0100

    Remove make from build scripts.

--- tools/build/linux-arm-gcc-debug.sh
+++ tools/build/linux-arm-gcc-debug.sh
@@ -2,4 +2,3 @@
 
 conan install .. --build missing -pr arm-linux-gnueabihf-gcc-9 -s build_type=Debug
 cmake .. -DPLATFORM=linux-arm
-make
--- tools/build/linux-arm-gcc-release.sh
+++ tools/build/linux-arm-gcc-release.sh
@@ -2,4 +2,3 @@
 
 conan install .. --build missing -pr arm-linux-gnueabihf-gcc-9 -s build_type=Release
 cmake .. -DPLATFORM=linux-arm -DCMAKE_BUILD_TYPE=Release
-make
