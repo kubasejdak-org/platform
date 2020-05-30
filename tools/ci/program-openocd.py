8b1aa0752acb54baa0df0c96af070ecf9a53f027
Kuba Sejdak <kuba.sejdak@gmail.com>
2020-05-30 22:30:37 +0200

    Enable baremetal CI tests 11.

--- tools/ci/program-openocd.py
+++ tools/ci/program-openocd.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
-import daemon
 import subprocess
 import sys
 
 cmd = ["openocd", "-f", "/usr/share/openocd/scripts/board/{}.cfg".format(sys.argv[1]), "-c", "program {} verify reset exit".format(sys.argv[2])]
 print(" ".join(cmd))
 
-with daemon.DaemonContext():
-    subprocess.Popen(cmd, stdout=subprocess.PIPE)
+subprocess.Popen(cmd, stdout=subprocess.PIPE)
