35394ad82672bd5ba8396958918bd5e2af8d0f0b
Kuba Sejdak <kuba.sejdak@gmail.com>
2021-01-14 21:42:12 +0100

    Check CI 3.

--- demo/init/baremetal-arm/platformInit.cpp
+++ demo/init/baremetal-arm/platformInit.cpp
@@ -32,56 +32,52 @@
 
 #include "platformInit.hpp"
 
-#include <stm32f4xx_gpio.h>
-#include <stm32f4xx_rcc.h>
-#include <stm32f4xx_usart.h>
+#include <stm32f4xx.h>
 
 #include <cstddef>
 #include <cstdint>
 
+UART_HandleTypeDef uart{};
+
 int consolePrint(const char* message, std::size_t size)
 {
-    for (std::size_t i = 0; i < size; ++i) {
-        while (USART_GetFlagStatus(UART4, USART_FLAG_TC) == RESET) { // NOLINT
-        }
-        USART_SendData(UART4, message[i]); // NOLINT
-    }
-
-    return size;
+    constexpr std::uint32_t cTimeout = 100;
+    auto result = HAL_UART_Transmit(&uart, (std::uint8_t*) message, size, cTimeout);
+    return (result == HAL_OK) ? int(size) : 0;
 }
 
 static void consoleInitGpio()
 {
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
-    GPIO_PinAFConfig(GPIOC, GPIO_PinSource10, GPIO_AF_UART4); // NOLINT
+    __HAL_RCC_GPIOC_CLK_ENABLE(); // NOLINT
 
     GPIO_InitTypeDef config{};
-    config.GPIO_Pin = GPIO_Pin_10;
-    config.GPIO_Mode = GPIO_Mode_AF;
-    config.GPIO_OType = GPIO_OType_PP;
-    config.GPIO_PuPd = GPIO_PuPd_UP;
-    config.GPIO_Speed = GPIO_Speed_100MHz;
-    GPIO_Init(GPIOC, &config); // NOLINT
+    config.Pin = GPIO_PIN_10;
+    config.Mode = GPIO_MODE_AF_PP;
+    config.Pull = GPIO_PULLUP;
+    config.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    config.Alternate = GPIO_AF8_UART4;
+    HAL_GPIO_Init(GPIOC, &config); // NOLINT
 }
 
 static void consoleInitUart()
 {
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART4, ENABLE);
+    __HAL_RCC_UART4_CLK_ENABLE(); // NOLINT
 
-    USART_InitTypeDef config{};
+    uart.Instance = UART4; // NOLINT
     constexpr std::uint32_t cConsoleBaudrate = 115200;
-    config.USART_BaudRate = cConsoleBaudrate;
-    config.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-    config.USART_Mode = USART_Mode_Tx;
-    config.USART_Parity = USART_Parity_No;
-    config.USART_StopBits = USART_StopBits_1;
-    config.USART_WordLength = USART_WordLength_8b;
-    USART_Init(UART4, &config); // NOLINT
-    USART_Cmd(UART4, ENABLE);   // NOLINT
+    uart.Init.BaudRate = cConsoleBaudrate;
+    uart.Init.WordLength = UART_WORDLENGTH_8B;
+    uart.Init.StopBits = UART_STOPBITS_1;
+    uart.Init.Parity = UART_PARITY_NONE;
+    uart.Init.Mode = UART_MODE_TX;
+    uart.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    uart.Init.OverSampling = UART_OVERSAMPLING_16;
+    HAL_UART_Init(&uart);
 }
 
 bool platformInit()
 {
+    HAL_Init();
     consoleInitGpio();
     consoleInitUart();
 
