c91e3fc3d9ab8652f5ee1b694cf7a8690ba39083
Kuba Sejdak <kuba.sejdak@gmail.com>
2022-05-07 08:10:09 +0200

    Fix CI.

--- app/linux/arm-linux-gnueabihf-clang.cmake
+++ app/linux/arm-linux-gnueabihf-clang.cmake
@@ -15,7 +15,7 @@ set(CMAKE_SIZE                      llvm-size${CMAKE_EXECUTABLE_SUFFIX} CACHE IN
 set(CMAKE_STRIP                     llvm-strip${CMAKE_EXECUTABLE_SUFFIX} CACHE INTERNAL "")
 set(CMAKE_GCOV                      llvm-cov${CMAKE_EXECUTABLE_SUFFIX} CACHE INTERNAL "")
 
-get_filename_component(TOOLCHAIN_ROOT "${LINUX_ARM_TOOLCHAIN_10_PATH}" DIRECTORY)
+get_filename_component(TOOLCHAIN_ROOT "${LINUX_ARM_TOOLCHAIN_PATH}" DIRECTORY)
 set(CMAKE_C_FLAGS                   "${APP_C_FLAGS} -target arm-none-linux-gnueabihf --gcc-toolchain=${TOOLCHAIN_ROOT} --sysroot=${TOOLCHAIN_ROOT}/arm-none-linux-gnueabihf/libc" CACHE INTERNAL "")
 set(CMAKE_CXX_FLAGS                 "${APP_CXX_FLAGS} ${CMAKE_C_FLAGS}" CACHE INTERNAL "")
 
@@ -24,7 +24,7 @@ set(CMAKE_C_FLAGS_RELEASE           "-O3 -DNDEBUG" CACHE INTERNAL "")
 set(CMAKE_CXX_FLAGS_DEBUG           "${CMAKE_C_FLAGS_DEBUG}" CACHE INTERNAL "")
 set(CMAKE_CXX_FLAGS_RELEASE         "${CMAKE_C_FLAGS_RELEASE}" CACHE INTERNAL "")
 
-set(CMAKE_EXE_LINKER_FLAGS          "${CMAKE_EXE_LINKER_FLAGS} -fuse-ld=${LINUX_ARM_TOOLCHAIN_10_PATH}arm-none-linux-gnueabihf-ld")
+set(CMAKE_EXE_LINKER_FLAGS          "${CMAKE_EXE_LINKER_FLAGS} -fuse-ld=${LINUX_ARM_TOOLCHAIN_PATH}arm-none-linux-gnueabihf-ld")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
