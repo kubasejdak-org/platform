ddff70a9626b93f023b8e6fdfeda2ebafc609882
Sejdak, Jakub <jakub.sejdak@trumpf.com>
2024-06-02 19:43:44 +0000

    Use find package for dependencies.

---
+++ lib/platform.cmake
@@ -0,0 +1,6 @@
+if (EXISTS ${CMAKE_CURRENT_LIST_DIR}/${PLATFORM})
+    include(${CMAKE_CURRENT_LIST_DIR}/${PLATFORM}/toolchain.cmake)
+    message(STATUS "platform: Using toolchain: ${PLATFORM}/${TOOLCHAIN}")
+else ()
+    message(FATAL_ERROR "platform: Invalid platform name: ${PLATFORM}")
+endif ()
