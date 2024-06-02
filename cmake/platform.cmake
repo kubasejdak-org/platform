e379e9f665c9bd4038354d0ca38b404793fb979c
Kuba Sejdak <kuba.sejdak+github@gmail.com>
2024-06-02 21:45:02 +0200

    Use find package for dependencies. (#12)

--- cmake/platform.cmake
+++
@@ -1,8 +0,0 @@
-if (NOT DEFINED PLATFORM)
-    set(DEFAULT_PLATFORM    linux)
-    message(STATUS "'PLATFORM' is not defined. Using '${DEFAULT_PLATFORM}'")
-    set(PLATFORM            ${DEFAULT_PLATFORM})
-endif ()
-
-# Setup platform toolchain file.
-include(lib/${PLATFORM}/toolchain.cmake)
