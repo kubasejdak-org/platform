543b8f86f478bc7649835e9df6d739b0bf473a39
Kuba Sejdak <kuba.sejdak+github@gmail.com>
2024-06-08 11:19:22 +0200

    Improve cmake. (#15)

--- cmake/modules/Findplatform-toolchain.cmake
+++ cmake/modules/Findplatform-toolchain.cmake
@@ -1,6 +1,1 @@
-option(PLATFORM "Platform to be used" linux)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(platform-toolchain DEFAULT_MSG)
-
 include (lib/platform.cmake)
