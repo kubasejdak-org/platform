0f739b874dcbddbf6745d4a707d9a0ba8e5266c0
Sejdak, Jakub <jakub.sejdak@trumpf.com>
2024-06-08 09:17:44 +0000

    Improve cmake.

--- lib/platform.cmake
+++ lib/platform.cmake
@@ -1,3 +1,5 @@
+option(PLATFORM "Platform to be used" linux)
+
 if (EXISTS ${CMAKE_CURRENT_LIST_DIR}/${PLATFORM})
     include(${CMAKE_CURRENT_LIST_DIR}/${PLATFORM}/toolchain.cmake)
     message(STATUS "platform: Using toolchain: ${PLATFORM}/${TOOLCHAIN}")
