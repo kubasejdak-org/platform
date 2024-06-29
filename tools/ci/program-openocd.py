0364888a3ebf8863788e9f91e6fd74ec76a6fe07
Sejdak, Jakub <jakub.sejdak@trumpf.com>
2024-06-29 12:22:59 +0000

    Improve CI scripts 2.

--- tools/ci/program-openocd.py
+++ tools/ci/program-openocd.py
@@ -4,7 +4,7 @@ import shlex
 import subprocess
 import sys
 
-cmd = f"openocd -f /usr/share/openocd/scripts/board/{sys.argv[1]}.cfg -c program {sys.argv[2]} verify reset exit"
+cmd = f"openocd -f /usr/share/openocd/scripts/board/{sys.argv[1]}.cfg -c \"program {sys.argv[2]} verify reset exit\""
 print(cmd)
 
 subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE)
