21e7e6c4bbad96bed3be1525fcbe7446a9b9c297
Kuba Sejdak <kuba.sejdak@gmail.com>
2025-04-15 22:24:48 +0200

    Adjust CMake and toolchain files.

---
+++ lib/toolchain/linux/coverage.cmake
@@ -0,0 +1,11 @@
+function (add_coverage_flags)
+    set(COVERAGE_FLAGS              "--coverage -fprofile-update=atomic")
+    string(REPLACE " " ";" COVERAGE_FLAGS_LIST ${COVERAGE_FLAGS})
+    add_compile_options(${COVERAGE_FLAGS_LIST})
+    set(CMAKE_C_LINK_FLAGS          "${CMAKE_C_LINK_FLAGS} ${COVERAGE_FLAGS}" CACHE INTERNAL "")
+    set(CMAKE_CXX_LINK_FLAGS        "${CMAKE_CXX_LINK_FLAGS} ${COVERAGE_FLAGS}" CACHE INTERNAL "")
+endfunction ()
+
+if (WITH_COVERAGE)
+    add_coverage_flags()
+endif ()
