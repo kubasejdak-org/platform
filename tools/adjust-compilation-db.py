81ef312a909c34626c4f20723beb9135b295552c
Kuba Sejdak <kuba.sejdak+github@gmail.com>
2026-01-30 23:12:52 +0100

    Refactor scripts: streamline pattern removal and argument handling in tools (#56)

--- tools/adjust-compilation-db.py
+++ tools/adjust-compilation-db.py
@@ -3,10 +3,7 @@
 import json
 
 COMPILATION_DB_FILE = "compile_commands.json"
-
-PATTERNS_TO_REMOVE = [
-    "_deps"
-]
+PATTERNS_TO_REMOVE = ["_deps"]
 
 with open(COMPILATION_DB_FILE) as file:
     compilation_db = json.load(file)
