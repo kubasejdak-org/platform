b14d1849499075eb0eab42ab0b0c33ee7d37d4cd
Kuba Sejdak <kuba.sejdak@gmail.com>
2026-01-30 23:10:49 +0100

    Refactor scripts: streamline pattern removal and argument handling in tools

--- tools/check-clang-tidy.sh
+++ tools/check-clang-tidy.sh
@@ -7,9 +7,7 @@ if [[ ! -f "compile_commands.json" ]]; then
     exit 1
 fi
 
-if [[ " $@ " =~ " -fix " ]]; then
-    EXTRA_ARGS="-fix"
-fi
+EXTRA_ARGS=$@
 
 PROJECT_ROOT=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")/..")
 
@@ -18,5 +16,5 @@ ${PROJECT_ROOT}/tools/adjust-compilation-db.py
 python3 ${PROJECT_ROOT}/tools/run-clang-tidy.py -quiet \
                                                 -header-filter=${PROJECT_ROOT}'(/app/|/examples/|/lib/|/tests/).*' \
                                                 -export-fixes=errors.yml \
-                                                -use-color \
+                                                -use-color 1 \
                                                 ${EXTRA_ARGS}
