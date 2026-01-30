25d13c62cc74984aa7b297d444426ffc9caf3869
Kuba Sejdak <kuba.sejdak+github@gmail.com>
2026-01-30 23:08:08 +0100

    Refactor toolchain files: remove sanitizer and coverage includes, update option descriptions (#55)
    * Refactor toolchain files: remove sanitizer and coverage includes, update option descriptions
    
    * Refactor toolchain files: update processor type to armv7 and add missing compiler paths

--- lib/toolchain/linux/toolchain.cmake
+++ lib/toolchain/linux/toolchain.cmake
@@ -2,9 +2,12 @@ if (NOT DEFINED CMAKE_TOOLCHAIN_FILE)
     set(DEFAULT_TOOLCHAIN       gcc)
 
     if (NOT DEFINED TOOLCHAIN)
-        message(STATUS "'TOOLCHAIN' is not defined. Using '${DEFAULT_TOOLCHAIN}'")
+        message(STATUS "platform: 'TOOLCHAIN' is not defined. Using '${DEFAULT_TOOLCHAIN}'.")
         set(TOOLCHAIN           ${DEFAULT_TOOLCHAIN})
     endif ()
 
     set(CMAKE_TOOLCHAIN_FILE    ${CMAKE_CURRENT_LIST_DIR}/${TOOLCHAIN}.cmake)
 endif ()
+
+include(${CMAKE_CURRENT_LIST_DIR}/sanitizers.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/coverage.cmake)
