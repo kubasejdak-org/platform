b85ba3a6573edaeab79e6dd3e0258dbd3c50dba3
Kuba Sejdak <kuba.sejdak@gmail.com>
2026-01-30 22:50:07 +0100

    Refactor toolchain files: remove sanitizer and coverage includes, update option descriptions

--- lib/toolchain/linux/coverage.cmake
+++ lib/toolchain/linux/coverage.cmake
@@ -1,4 +1,4 @@
-option(WITH_COVERAGE "Compile with coverage support" OFF)
+option(WITH_COVERAGE "platform: Compile with coverage support" OFF)
 
 function (add_coverage_flags)
     set(COVERAGE_FLAGS              "--coverage -fprofile-update=atomic")
