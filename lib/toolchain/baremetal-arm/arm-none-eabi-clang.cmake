b96a3fbf42198949072771493b7fc7fbbfc0d635
Kuba Sejdak <kuba.sejdak@gmail.com>
2026-04-13 22:53:41 +0200

    Add clang support for baremetal

---
+++ lib/toolchain/baremetal-arm/arm-none-eabi-clang.cmake
@@ -0,0 +1,38 @@
+set(CMAKE_SYSTEM_NAME               Generic)
+set(CMAKE_SYSTEM_PROCESSOR          armv7)
+
+# Without this flag CMake is not able to pass the compiler sanity check.
+set(CMAKE_TRY_COMPILE_TARGET_TYPE   STATIC_LIBRARY)
+
+set(CMAKE_AR                        llvm-ar CACHE FILEPATH "")
+set(CMAKE_ASM_COMPILER              clang CACHE FILEPATH "")
+set(CMAKE_C_COMPILER                clang CACHE FILEPATH "")
+set(CMAKE_CXX_COMPILER              clang++ CACHE FILEPATH "")
+set(CMAKE_LINKER                    lld CACHE FILEPATH "")
+set(CMAKE_OBJCOPY                   llvm-objcopy CACHE FILEPATH "")
+set(CMAKE_RANLIB                    llvm-ranlib CACHE FILEPATH "")
+set(CMAKE_SIZE                      llvm-size CACHE FILEPATH "")
+set(CMAKE_STRIP                     llvm-strip CACHE FILEPATH "")
+set(CMAKE_GCOV                      llvm-cov CACHE FILEPATH "")
+
+if (DEFINED BAREMETAL_ARM_TOOLCHAIN_PATH)
+    set(GCC_TOOLCHAIN_FLAG          "--gcc-toolchain=${BAREMETAL_ARM_TOOLCHAIN_PATH}")
+endif ()
+
+set(COMMON_FLAGS                    "-target arm-none-eabi ${GCC_TOOLCHAIN_FLAG} -fdata-sections -ffunction-sections" CACHE INTERNAL "")
+set(PLATFORM_C_FLAGS                "${COMMON_FLAGS} ${APP_C_FLAGS}" CACHE INTERNAL "")
+set(PLATFORM_CXX_FLAGS              "${COMMON_FLAGS} ${APP_CXX_FLAGS} -fno-exceptions" CACHE INTERNAL "")
+set(CMAKE_C_FLAGS                   "${PLATFORM_C_FLAGS}" CACHE INTERNAL "")
+set(CMAKE_CXX_FLAGS                 "${PLATFORM_CXX_FLAGS}" CACHE INTERNAL "")
+
+set(CMAKE_C_FLAGS_DEBUG             "-Os -g" CACHE INTERNAL "")
+set(CMAKE_C_FLAGS_RELEASE           "-Os -DNDEBUG" CACHE INTERNAL "")
+set(CMAKE_CXX_FLAGS_DEBUG           "${CMAKE_C_FLAGS_DEBUG}" CACHE INTERNAL "")
+set(CMAKE_CXX_FLAGS_RELEASE         "${CMAKE_C_FLAGS_RELEASE}" CACHE INTERNAL "")
+
+set(PLATFORM_EXE_LINKER_FLAGS       "" CACHE INTERNAL "")
+set(CMAKE_EXE_LINKER_FLAGS          "${PLATFORM_EXE_LINKER_FLAGS}" CACHE INTERNAL "")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
--- tools/adjust-compilation-db.py
+++ tools/adjust-compilation-db.py
@@ -3,7 +3,7 @@
 import json
 
 COMPILATION_DB_FILE = "compile_commands.json"
-PATTERNS_TO_REMOVE = ["_deps"]
+PATTERNS_TO_REMOVE = ["_deps", "freertos-"]
 
 with open(COMPILATION_DB_FILE) as file:
     compilation_db = json.load(file)
